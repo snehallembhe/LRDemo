8f0c2963e6fb25a32c84cd6677ee9079e4ac65d6
unknown <snehal.lembhe@CARDINALHEALTH>
2020-12-07 16:32:48 +0530

    this is commit script

---
+++ Action.c
@@ -0,0 +1,18 @@
+Action()
+{
+	
+	web_custom_request("web_custom_request",
+		"URL=www.google.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	
+	
+	
+	return 0;
+}
